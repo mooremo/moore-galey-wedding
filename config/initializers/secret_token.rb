3326ec2f6f029434a36b68c4cf73d8934fd624d2
Matt Moore <mmoore@groupon.com>
2013-07-15 17:20:59 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeddingWebsite::Application.config.secret_key_base = 'de51ffdacaae602007950bd0cb631efa5bf338ffb706f8da7f0b251600115b9eef454f8b845e25d2b97b1e131653a5e774334499eb0ca17297e72bfe1ad470a0'
